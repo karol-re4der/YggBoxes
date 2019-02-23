a20d4cffc08a408029619dc312a44b658f54d461
re4der <re4der@Cantropol>
2019-02-23 14:08:17 +0100

    Output adjusted to fit task specification more.

--- src/main/java/com/theglorious/ygg/Ygg.java
+++ src/main/java/com/theglorious/ygg/Ygg.java
@@ -8,9 +8,11 @@ package com.theglorious.ygg;
 public class Ygg {
     public static void main(String[] args){
 	Solution sol = new Simulation(10000000);
-	System.out.println("Simulated reward per round: "+sol.solve(new Boxes()));
+	//System.out.print("Simulated reward per round: ");
+	System.out.println(sol.solve(new Boxes()));
 	
 	sol = new Calculation();
-	System.out.println("Calculated reward per round: "+sol.solve(new Boxes()));
+	//System.out.print("Calculated reward per round: ");
+	System.out.println(sol.solve(new Boxes()));
     }
 }
